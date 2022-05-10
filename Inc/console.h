e2dd7ad857bc2b8ff5675b506f70ffd9008277cb
Nefastor <20577371+Nefastor@users.noreply.github.com>
2022-05-10 21:02:02 +0200

    Initial commit after file creation

---
+++ Inc/console.h
@@ -0,0 +1,13 @@
+/*
+ * console.h
+ *
+ *  Created on: May 10, 2022
+ *      Author: Nefastor
+ */
+
+#ifndef INC_CONSOLE_H_
+#define INC_CONSOLE_H_
+
+
+
+#endif /* INC_CONSOLE_H_ */
---
+++ Src/console.c
@@ -0,0 +1,8 @@
+/*
+ * console.c
+ *
+ *  Created on: May 10, 2022
+ *      Author: Nefastor
+ */
+
+
